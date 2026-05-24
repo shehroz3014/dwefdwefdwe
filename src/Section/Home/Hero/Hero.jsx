3ea3a21df4c91553c57a45377a7b4fb82fd55c34
shehroz khan <sk1331806@gamil.com>
2026-05-24 20:42:46 +0500

    update project

--- src/Section/Home/Hero/Hero.jsx
+++ src/Section/Home/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className='container mx-auto flex flex-col md:flex-row items-center p-4 flex-1  py-20'>
                 <div className=''>
                     <span className='font-bold text-lg text-my-pink'>Best Furniture For Your Castle....</span>
-                    <h2 className='fnt font-bold text-5xl py-8'>New Furniture Collection <br />Trends in 2020</h2>
+                    <h2 className='fnt font-bold text-5xl py-8'>New Furniture Collection <br />Trends in 2026</h2>
                     <p className='text-my-subText font-bold text-lg mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.</p>
                     <Button
                         className="bg-my-pink px-6 py-3 rounded-sm text-white fnt font-semibold"
